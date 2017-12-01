bb2ed2465bed30ff7174d1bfa3b4fdcf60566a75
Pavel Zagrebelin <pavel@zagrebelin.ru>
2017-12-01 18:23:59 +0500

    day 16, part 2

--- day16.py
+++ day16.py
@@ -1,24 +1,51 @@
 import re
 
-class Sue:
-    def __init__(self, params):
-        self.params = {k:int(v) for k, v in params.items()}
 
-    def match(self, criteria):
-        for name, value in criteria.items():
+class SuePartA:
+    def __init__(self, idx, params):
+        self.idx = idx
+        self.params = {k: int(v) for k, v in params.items()}
+
+    def __eq__(self, other):
+        for name, value in other.params.items():
             self_value = self.params.get(name, None)
-            if self_value is not None and value != self_value:
+            if self_value is None:
+                continue
+            if value != self_value:
                 return False
         return True
 
-sues= {}
-for line in open('day16.txt'):
-    match = re.match('Sue (\d+): (.*)', line)
-    idx, criterias = match.groups()
-    criterias = dict(re.findall('(\w+): (\d+)', line))
-    sues[idx] = Sue(criterias)
+    @classmethod
+    def from_line(cls, line):
+        match = re.match('Sue (\d+): (.*)', line)
+        idx, criterias = match.groups()
+        criterias = dict(re.findall('(\w+): (\d+)', line))
+        return cls(idx, criterias)
 
-need = {
+
+class SuePartB(SuePartA):
+    def __init__(self, idx, params):
+        super().__init__(idx, params)
+
+    def __eq__(self, other):
+        for name, value in other.params.items():
+            self_value = self.params.get(name, None)
+            if self_value is None:
+                continue
+            if name in ('cats', 'trees'):
+                if value > self_value:
+                    return False
+            elif name in ('pomeranians', 'goldfish'):
+                if value < self_value:
+                    return False
+            elif value != self_value:
+                return False
+        return True
+
+
+SueClass = SuePartB
+
+need = SueClass(-1, {
     'children': 3,
     'cats': 7,
     'samoyeds': 2,
@@ -28,9 +55,9 @@ need = {
     'goldfish': 5,
     'trees': 3,
     'cars': 2,
-    'perfumes': 1,
-}
+    'perfumes': 1, })
 
-for idx, sue in sues.items():
-    if sue.match(need):
-        print(idx)
+for line in open('day16.txt'):
+    sue = SueClass.from_line(line)
+    if sue == need:
+        print(sue.idx)
